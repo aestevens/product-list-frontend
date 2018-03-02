ba608712125f406d4159abb44c87262211916df7
Austin Stevens <austinestevens@outlook.com>
2018-03-02 12:10:58 -0500

    Initial commit with starter dependencies.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+    );
+  }
+}
+
+export default App;
